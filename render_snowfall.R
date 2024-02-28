063b9725d457c4f4e9d9596aef522875cc108a84
Ben Matheson <benmatheson@Bens-M2-MacBook-Pro.local>
2024-02-28 13:24:42 -0900

    initial

---
+++ render_snowfall.R
@@ -0,0 +1,16 @@
+
+# 
+setwd("projects/snowfall")
+# setwd("/")
+options(scipen=999)
+getwd()
+
+# install.packages('knitr')
+# install.packages('rmarkdown')
+library(knitr)
+library(rmarkdown)
+
+# knitr::knit("snowfall_data.Rmd", output = "snowfall.html")
+render(input="snowfall_data.Rmd")
+
+
