dcde20e43cb6566f53bc00346820e4ef647ea113
benmatheson <mathesonben@gmail.com>
2024-02-29 21:29:20 -0900

    httr

--- render_snowfall.R
+++ render_snowfall.R
@@ -12,6 +12,7 @@ library(knitr)
 library(rmarkdown)
 
 # knitr::knit("snowfall_data.Rmd", output = "snowfall.html")
-render(input="snowfall_data.Rmd")
+# render(input="snowfall_data.Rmd")
+render(input="snowfall_data.Rmd", output="docs/index.html")
 
 
